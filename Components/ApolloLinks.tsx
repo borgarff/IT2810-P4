2d3de8187044f9c2cc7e6fce718ffee045bc219b
Borgar Føll Flytør <borgarff@stud.ntnu.no>
2020-11-17 23:56:23 +0100

    Endret link til backend

--- Components/ApolloLinks.tsx
+++ Components/ApolloLinks.tsx
@@ -4,7 +4,7 @@ export const cache = new InMemoryCache()
 
 //Hyperlink for the application
 export const client = new ApolloClient({
-  uri: 'http://192.168.1.57:4000/', //Husk å sjekk denne 
+  uri: 'http://localhost:4000/', //Husk å sjekk denne 
   cache,
   defaultOptions: { watchQuery: { fetchPolicy: 'cache-and-network' } },
 });
